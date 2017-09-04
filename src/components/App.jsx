8cdf7de2d5a9f1b1a619cc5574a176f5bad24330
Greg Trowbridge <gjtrowbridge@gmail.com>
2017-09-04 14:50:11 -0700

    Add initial Ace editor integration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import Game from 'Components/Game';
+
+// Ace editor
+import 'brace';
+import AceEditor from 'react-ace';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
+
 // CSS
 import 'CSS/App.scss';
+
+function onChange(newValue) {
+  console.log('change', newValue);
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Game</h1>
+        <h2>Code Your Brain Here</h2>
+        <AceEditor
+          mode="javascript"
+          theme="monokai"
+          onChange={onChange}
+          name="UNIQUE_ID_OF_DIV"
+          fontSize="18px"
+          editorProps={{
+            $blockScrolling: true,
+          }}
+        />
+        <h2>Watch Your Brain In Action Here</h2>
         <Game height={400} width={400} />
       </div>
     );
