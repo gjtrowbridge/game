c5b0ef734e0c5f2823b7613ade8430dcb42e2c26
Greg Trowbridge <gjtrowbridge@gmail.com>
2017-09-04 15:10:32 -0700

    Update style of App.jsx to show two columns

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,19 +18,25 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>Code Your Brain Here</h2>
-        <AceEditor
-          mode="javascript"
-          theme="monokai"
-          onChange={onChange}
-          name="UNIQUE_ID_OF_DIV"
-          fontSize="18px"
-          editorProps={{
-            $blockScrolling: true,
-          }}
-        />
-        <h2>Watch Your Brain In Action Here</h2>
-        <Game height={400} width={400} />
+        <div className="column editor">
+          <h2>Code Your Brain Here</h2>
+          <AceEditor
+            mode="javascript"
+            theme="monokai"
+            onChange={onChange}
+            name="UNIQUE_ID_OF_DIV"
+            fontSize="18px"
+            width="500px"
+            height="500px"
+            editorProps={{
+              $blockScrolling: true,
+            }}
+          />
+        </div>
+        <div className="column game">
+          <h2>Watch Your Brain In Action Here</h2>
+          <Game height={500} width={500} />
+        </div>
       </div>
     );
   }
